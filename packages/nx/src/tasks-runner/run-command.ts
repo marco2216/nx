7e7bab490fd6c9527cc5c1670392f08d6cbf75af
Craigory Coppola <craigorycoppola@gmail.com>
2026-04-08 22:43:32 -0400

    fix(core): move back to task-orchestrator for detection

--- packages/nx/src/tasks-runner/run-command.ts
+++ packages/nx/src/tasks-runner/run-command.ts
@@ -18,14 +18,7 @@ import {
   getTaskDetails,
   hashTasksThatDoNotDependOnOutputsOfOtherTasks,
 } from '../hasher/hash-task';
-import {
-  hashArray,
-  IS_WASM,
-  logDebug,
-  RunMode,
-  TaskInvocationTracker,
-} from '../native';
-import { getLocalDbConnection } from '../utils/db-connection';
+import { hashArray, logDebug, RunMode } from '../native';
 import {
   runPostTasksExecution,
   runPreTasksExecution,
@@ -395,14 +388,6 @@ function createTaskGraphAndRunValidations(
     loadDotEnvFiles: boolean;
   }
 ) {
-  const taskInvocationTracker = !IS_WASM
-    ? new TaskInvocationTracker(
-        getLocalDbConnection(),
-        Number(process.env.NX_INVOCATION_ROOT_PID ?? process.pid)
-      )
-    : undefined;
-  taskInvocationTracker?.cleanupStale();
-
   const taskGraph = createTaskGraph(
     projectGraph,
     extraTargetDependencies,
@@ -410,8 +395,7 @@ function createTaskGraphAndRunValidations(
     nxArgs.targets,
     nxArgs.configuration,
     overrides,
-    extraOptions.excludeTaskDependencies,
-    taskInvocationTracker
+    extraOptions.excludeTaskDependencies
   );
 
   assertTaskGraphDoesNotContainInvalidTargets(taskGraph);
